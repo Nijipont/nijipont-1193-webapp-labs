bed11d9ae760ce0a4b9571858cdac9d714df4ac4
nijipont <nijipont.h@kkumail.com>
2025-09-15 16:29:43 +0700

    Lab 7 P2 update and output

--- Lab 7/lib/P2.js
+++ Lab 7/lib/P2.js
@@ -1,7 +1,6 @@
 fetch("files/students.json").then((response) => response.json())
     .then((data) => {
         const wclass = data.wclass;
-        console.log(wclass);
         for (let person of wclass) {
             const list_student = document.createElement("li");
             list_student.innerHTML = `${person.student["@attributes"].id} <b>${person.student.name}</b> has skills <i>${person.student.skill}</i>`;
